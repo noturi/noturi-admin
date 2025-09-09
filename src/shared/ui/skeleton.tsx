8f4f5cdc7b21961aa52c2a1eaf8f58eddb3bb08a
dokimion24 <leecy3610@gmail.com>
2025-09-09 20:31:07 +0900

    feat: 경로 수정으로 인해 cn 임포트 경로 변경

--- src/shared/ui/skeleton.tsx
+++ src/shared/ui/skeleton.tsx
@@ -1,4 +1,4 @@
-import { cn } from '@/shared/lib';
+import { cn } from '../lib';
 
 function Skeleton({ className, ...props }: React.ComponentProps<'div'>) {
   return <div data-slot="skeleton" className={cn('bg-accent animate-pulse rounded-md', className)} {...props} />;
--- src/shared/ui/table.tsx
+++ src/shared/ui/table.tsx
@@ -1,8 +1,7 @@
 'use client';
 
 import * as React from 'react';
-
-import { cn } from '@/shared/lib';
+import { cn } from '../lib';
 
 function Table({ className, ...props }: React.ComponentProps<'table'>) {
   return (
