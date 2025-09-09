22865596ffdd4cca447bed58d5c139a2f8498784
dokimion24 <leecy3610@gmail.com>
2025-09-09 20:29:20 +0900

    feat: 대시보드 사용자 경로 수정 및 주석 제거

--- src/shared/lib/model/routes.ts
+++ src/shared/lib/model/routes.ts
@@ -1,13 +1,11 @@
 export const ROUTES = {
-  // 인증
   AUTH: {
     LOGIN: '/auth/login',
   },
 
-  // 대시보드
   DASHBOARD: {
     HOME: '/dashboard',
-    USERS: '/dashboard/users',
+    USER: '/dashboard/user',
     CATEGORIES: '/dashboard/categories',
     SETTINGS: '/dashboard/settings',
   },
