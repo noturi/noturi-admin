dbd913c7143296fb8bb7135ffdc69692400fac60
dokimion24 <leecy3610@gmail.com>
2025-09-12 16:50:35 +0900

    feat: 서버 API 관련 코드 제거 및 클라이언트 API만 남김

--- src/shared/api/api.ts
+++ src/shared/api/api.ts
@@ -1,19 +1,8 @@
 import { END_POINT } from '@/shared/lib';
 import { EnhancedFetch } from './enhanced-fetch';
 
-export const serverApi = new EnhancedFetch(END_POINT.BASE_URL || '', {
-  headers: {
-    'Content-Type': 'application/json',
-  },
-});
-
 export const clientApi = new EnhancedFetch(END_POINT.BASE_URL || '', {
   headers: {
     'Content-Type': 'application/json',
   },
 });
-
-export const api = {
-  client: clientApi,
-  server: serverApi,
-};
