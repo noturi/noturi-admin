bb49934c0c5371f68a22f40816b47c3eef280ce8
dokimion24 <leecy3610@gmail.com>
2025-09-12 16:51:12 +0900

    feat: 서버 API를 위한 EnhancedFetch 구현 및 쿠키에서 인증 토큰 추출 기능 추가

---
+++ src/shared/api/server-api.ts
@@ -0,0 +1,27 @@
+import { cookies } from 'next/headers';
+import { END_POINT } from '@/shared/lib';
+import { EnhancedFetch } from './enhanced-fetch';
+
+async function getTokenFromCookies() {
+  const cookieStore = await cookies();
+  return cookieStore.get('auth-token')?.value;
+}
+
+export const serverApi = new EnhancedFetch(
+  (END_POINT.BASE_URL || '') + '/admin',
+  {
+    headers: {
+      'Content-Type': 'application/json',
+    },
+  },
+  async (options) => {
+    const token = await getTokenFromCookies();
+    return {
+      ...options,
+      headers: {
+        ...options.headers,
+        ...(token && { Authorization: `Bearer ${token}` }),
+      },
+    };
+  }
+);
