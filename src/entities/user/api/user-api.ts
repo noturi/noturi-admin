41019b9700b6217be53733f7b5bdadd71fa81e4b
dokimion24 <leecy3610@gmail.com>
2025-09-15 17:03:46 +0900

    feat: 사용자 목록 API 호출 함수 이름 변경 및 반환 타입 개선

--- src/entities/user/api/user-api.ts
+++ src/entities/user/api/user-api.ts
@@ -1,12 +1,8 @@
 import { serverApi } from '@/shared/api/server-api';
 import type { UserListResponse, UserQueryParams } from '../model/types';
 
-export const userApi = {
-  getList: async (params?: UserQueryParams): Promise<UserListResponse> => {
-    return serverApi
-      .get('/users', {
-        searchParams: params as Record<string, string>,
-      })
-      .then((res) => res.json<UserListResponse>());
-  },
+export const getUserList = async (params?: UserQueryParams) => {
+  return serverApi.get<UserListResponse>('/users', {
+    searchParams: params,
+  });
 };
