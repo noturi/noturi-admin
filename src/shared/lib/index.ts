2d51884f9614b05fe9d2cc529807223defa7a10f
dokimion24 <leecy3610@gmail.com>
2025-10-02 10:24:55 +0900

    feat: index.ts 파일에서 HTTP 오류 처리 관련 내보내기 경로 수정

--- src/shared/lib/index.ts
+++ src/shared/lib/index.ts
@@ -12,7 +12,7 @@ export * from './end-point';
 export * from './utils/auth';
 export * from './utils/auth.server';
 export * from './utils/format-time';
-export * from './utils/http-error';
+
 export * from './utils/query-keys';
 export * from './utils/search-params';
 export * from './utils/role';
