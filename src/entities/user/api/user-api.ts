a215ff53b17af372d6fab6ef3aed7b039391c4bc
dokimion24 <leecy3610@gmail.com>
2025-10-02 17:28:09 +0900

    chore: 사용자 목록 요청 시 콘솔 로그 제거

--- src/entities/user/api/user-api.ts
+++ src/entities/user/api/user-api.ts
@@ -2,7 +2,6 @@ import { serverApi } from '@/shared/api/server-api';
 import type { UserListResponse, UserQueryParams } from '../model/types';
 
 export const getUserList = async (params?: UserQueryParams) => {
-  console.log('Requesting /users with params:', params);
   return serverApi.get<UserListResponse>('/users', {
     searchParams: params,
   });
